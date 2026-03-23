2dc521183036ddfc8de93e5cf5ce20791d0ca9b4
Betta_Fish <zxbmmmmmmmmm@qq.com>
2026-03-23 22:43:01 +0800

    fix(Sample): fix Android starting

--- samples/SmoothScroll.Avalonia.Sample.Android/MainActivity.cs
+++ samples/SmoothScroll.Avalonia.Sample.Android/MainActivity.cs
@@ -11,10 +11,12 @@ namespace SmoothScroll.Avalonia.Sample.Android;
     Theme = "@style/MyTheme.NoActionBar",
     Icon = "@drawable/icon",
     MainLauncher = true,
+    Exported = true,
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
 public class MainActivity : AvaloniaMainActivity
 {
 }
+[Application]
 public class Application : AvaloniaAndroidApplication<App>
 {
     protected Application(nint javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
