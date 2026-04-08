e624899d52e598130c77c7449bb0bd1a414aedce
Betta_Fish <zxbmmmmmmmmm@outlook.com>
2026-04-08 15:28:02 +0800

    feat(Controls): add styles

---
+++ src/SmoothScroll.Avalonia.Controls/ScrollViewDefaultTheme.axaml.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Avalonia.Styling;
+
+namespace SmoothScroll.Avalonia.Controls;
+
+public class ScrollViewDefaultTheme : Styles
+{
+}
---
+++ src/SmoothScroll.Avalonia.Controls/ScrollViewerSmoothTheme.axaml.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Avalonia.Styling;
+
+namespace SmoothScroll.Avalonia.Controls;
+
+public class ScrollViewerSmoothTheme : Styles
+{
+}
