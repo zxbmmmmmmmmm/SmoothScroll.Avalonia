9ccc39e20305ea79c673484a45de63c46c2eff2a
Betta_Fish <zxbmmmmmmmmm@qq.com>
2026-04-15 18:06:10 +0800

    feat(Sample): add github link

--- samples/SmoothScroll.Avalonia.Sample/ViewModels/TextBoxViewModel.cs
+++ samples/SmoothScroll.Avalonia.Sample/ViewModels/TextBoxViewModel.cs
@@ -8,5 +8,5 @@ public class TextBoxViewModel
     private const string Lorem = """
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         """;
-    public string Text { get; set; } = string.Join(Environment.NewLine, Enumerable.Range(1, 100).Select(i => $"Line {i}: {Lorem}"));
+    public string Text { get; set; } = string.Join(Environment.NewLine, Enumerable.Range(1, OperatingSystem.IsBrowser() ? 50 : 100).Select(i => $"Line {i}: {Lorem}"));
 }
--- samples/SmoothScroll.Avalonia.Sample/Views/MainView.axaml.cs
+++ samples/SmoothScroll.Avalonia.Sample/Views/MainView.axaml.cs
@@ -1,11 +1,21 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using Avalonia.Controls.ApplicationLifetimes;
 
 namespace SmoothScroll.Avalonia.Sample.Views;
 
-public partial class MainView : UserControl
+public partial class MainView : ContentPage
 {
     public MainView()
     {
         InitializeComponent();
+    }
+
+    private void Button_Click(object? sender, RoutedEventArgs e)
+    {
+        var launcher = TopLevel.GetTopLevel(this)?.Launcher;
+        launcher?.LaunchUriAsync(new Uri("https://github.com/zxbmmmmmmmmm/SmoothScroll.Avalonia"));
     }
 }
