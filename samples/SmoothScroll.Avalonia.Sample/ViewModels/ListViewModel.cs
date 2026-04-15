fb4ad2ef43dd5ba5f4fa4ab4eb7192c47ca4c5cd
Betta_Fish <zxbmmmmmmmmm@outlook.com>
2026-04-15 14:51:36 +0800

    feat(ci): add github page

--- samples/SmoothScroll.Avalonia.Sample/ViewModels/ListViewModel.cs
+++ samples/SmoothScroll.Avalonia.Sample/ViewModels/ListViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace SmoothScroll.Avalonia.Sample.ViewModels;
 
 public class ListViewModel : ViewModelBase
 {
-    public ObservableCollection<int> Items { get; } = new(Enumerable.Range(1, 1000));
+    public ObservableCollection<int> Items { get; } = new(Enumerable.Range(1, OperatingSystem.IsBrowser() ? 100 : 1000));
 }
