6b70aff6abba62d87473954e7a0e6f6db8fbfc11
Betta_Fish <zxbmmmmmmmmm@qq.com>
2026-05-17 22:54:48 +0800

    wip

---
+++ src/SmoothScroll.Avalonia.Interaction/InertiaAxisHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace SmoothScroll.Avalonia.Interaction;
+
+public class InertiaAxisHelper
+{
+    private const double DecayRate = 0.95;
+    
+    public double StartVelocity { get; set; }
+
+    public double NaturalRestingValue { get; set; }
+
+    public double ModifiedRestingValue { get; set; }
+
+    public double CurrentValue { get; set; }
+
+
+    private readonly Stopwatch _stopwatch = new();
+
+    public double Tick()
+    {
+        var elapsed = _stopwatch.ElapsedMilliseconds;
+
+    }
+
+    public void Initialize(
+        double currentValue,
+        double startVelocity)
+    {
+        _stopwatch.Start();
+        CurrentValue = currentValue;
+        StartVelocity = startVelocity;
+    }
+}
